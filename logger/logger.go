134b48e9cf2c13690d747b3ebacfbb41a3d304b2
Adam Vu <gvu0110@gmail.com>
2021-05-24 00:23:14 -0400

    Added public logger library

---
+++ logger/logger.go
@@ -0,0 +1,100 @@
+package logger
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	envLogLevel  = "LOG_LEVEL"
+	envLogOutput = "LOG_OUTPUT"
+)
+
+var (
+	log logger
+)
+
+type loggerInterface interface {
+	MySQLLogger(...interface{})
+	ElasticSearchLogger(string, ...interface{})
+}
+
+type logger struct {
+	log *zap.Logger
+}
+
+func init() {
+	logConfig := zap.Config{
+		OutputPaths: []string{getOutput()},
+		Level:       zap.NewAtomicLevelAt(getLevel()),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "message",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+
+	var err error
+	if log.log, err = logConfig.Build(); err != nil {
+		panic(err)
+	}
+}
+
+func GetLogger() loggerInterface {
+	return log
+}
+
+func getOutput() string {
+
+	output := strings.TrimSpace(os.Getenv(envLogOutput))
+	if output == "" {
+		return "stdout"
+	}
+	return output
+}
+
+func getLevel() zapcore.Level {
+	switch os.Getenv(envLogLevel) {
+	case "debug":
+		return zap.DebugLevel
+	case "info":
+		return zap.InfoLevel
+	case "error":
+		return zap.ErrorLevel
+	default:
+		return zap.InfoLevel
+	}
+}
+
+// Implement the MySQL Logger interface
+func (l logger) MySQLLogger(v ...interface{}) {
+	Info(fmt.Sprintf("%v", v...))
+}
+
+// Implement the Elastic Logger interface
+func (l logger) ElasticSearchLogger(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+	} else {
+		Info(fmt.Sprintf(format, v...))
+	}
+}
+
+func Info(msg string, tags ...zap.Field) {
+	log.log.Info(msg, tags...)
+	log.log.Sync()
+}
+
+func Error(msg string, err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.log.Info(msg, tags...)
+	log.log.Sync()
+}
