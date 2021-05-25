fe0a2751dbfabf3229b6a8e00fbb366ac4fcc8e6
Adam Vu <gvu0110@gmail.com>
2021-05-25 00:21:59 -0400

    Improved rest error library by adding error interface

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -1,49 +1,83 @@
 package rest_errors
 
 import (
+	"fmt"
 	"net/http"
 )
 
-// RESTError struct is an custom REST error is used for entire microservice
-type RESTError struct {
-	StatusCode int           `json:"status_code"`
-	Message    string        `json:"message"`
-	Error      string        `json:"error"`
-	Causes     []interface{} `json:"causes"`
+type RESTError interface {
+	StatusCode() int
+	Message() string
+	Error() string
+	Causes() []interface{}
+}
+
+// restError struct is an custom REST error is used for entire microservice
+type restError struct {
+	statusCode int           `json:"status_code"`
+	message    string        `json:"message"`
+	error      string        `json:"error"`
+	causes     []interface{} `json:"causes"`
+}
+
+// Implement built-in Error go function
+func (e restError) Error() string {
+	return fmt.Sprintf("message: %s - status_code: %d - error: %s - causes: %v", e.message, e.statusCode, e.error, e.causes)
+}
+
+func (e restError) StatusCode() int {
+	return e.statusCode
+}
+
+func (e restError) Message() string {
+	return e.message
+}
+
+func (e restError) Causes() []interface{} {
+	return e.causes
+}
+
+func NewRESTError(message string, statusCode int, err string, causes []interface{}) RESTError {
+	return restError{
+		statusCode: statusCode,
+		message:    message,
+		error:      err,
+		causes:     causes,
+	}
 }
 
 // NewBadRequestRESTError creates a new bad request REST error
-func NewBadRequestRESTError(message string) *RESTError {
-	return &RESTError{
-		StatusCode: http.StatusBadRequest,
-		Message:    message,
-		Error:      "BAD REQUEST",
+func NewBadRequestRESTError(message string) RESTError {
+	return restError{
+		statusCode: http.StatusBadRequest,
+		message:    message,
+		error:      "bad_request",
 	}
 }
 
 // NewNotFoundRESTError creates a new not found REST error
-func NewNotFoundRESTError(message string) *RESTError {
-	return &RESTError{
-		StatusCode: http.StatusNotFound,
-		Message:    message,
-		Error:      "NOT FOUND",
+func NewNotFoundRESTError(message string) RESTError {
+	return restError{
+		statusCode: http.StatusNotFound,
+		message:    message,
+		error:      "not_found",
 	}
 }
 
-func NewUnauthorizedRESTError(message string) *RESTError {
-	return &RESTError{
-		StatusCode: http.StatusUnauthorized,
-		Message:    message,
-		Error:      "UNAUTHORIZED",
+func NewUnauthorizedRESTError(message string) RESTError {
+	return restError{
+		statusCode: http.StatusUnauthorized,
+		message:    message,
+		error:      "unauthorized",
 	}
 }
 
 // NewInternalServerError creates a new internal server REST error
-func NewInternalServerRESTError(message string, err error) *RESTError {
-	return &RESTError{
-		StatusCode: http.StatusInternalServerError,
-		Message:    message,
-		Error:      "INTERNAL SERVER ERROR",
-		Causes:     []interface{}{err.Error()},
+func NewInternalServerRESTError(message string, err error) RESTError {
+	return restError{
+		statusCode: http.StatusInternalServerError,
+		message:    message,
+		error:      "internal_server_error",
+		causes:     []interface{}{err.Error()},
 	}
 }
--- rest_errors/rest_errors_test.go
+++ rest_errors/rest_errors_test.go
@@ -8,30 +8,50 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNewRESTError(t *testing.T) {
+	err := NewRESTError("This is the test message", http.StatusForbidden, "forbidden", []interface{}{"Error causes"})
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusForbidden, err.StatusCode())
+	assert.EqualValues(t, "This is the test message", err.Message())
+	assert.EqualValues(t, "message: This is the test message - status_code: 403 - error: forbidden - causes: [Error causes]", err.Error())
+
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes()))
+	assert.EqualValues(t, "Error causes", err.Causes()[0])
+}
+
 func TestNewInternalServerRESTError(t *testing.T) {
 	err := NewInternalServerRESTError("This is the test message", errors.New("Error causes"))
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
-	assert.EqualValues(t, "This is the test message", err.Message)
-	assert.EqualValues(t, "INTERNAL SERVER ERROR", err.Error)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode())
+	assert.EqualValues(t, "This is the test message", err.Message())
+	assert.EqualValues(t, "message: This is the test message - status_code: 500 - error: internal_server_error - causes: [Error causes]", err.Error())
 
-	assert.NotNil(t, err.Causes)
-	assert.EqualValues(t, 1, len(err.Causes))
-	assert.EqualValues(t, "Error causes", err.Causes[0])
+	assert.NotNil(t, err.Causes())
+	assert.EqualValues(t, 1, len(err.Causes()))
+	assert.EqualValues(t, "Error causes", err.Causes()[0])
 }
 
 func TestNewNotFoundRESTError(t *testing.T) {
 	err := NewNotFoundRESTError("This is the test message")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
-	assert.EqualValues(t, "This is the test message", err.Message)
-	assert.EqualValues(t, "NOT FOUND", err.Error)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode())
+	assert.EqualValues(t, "This is the test message", err.Message())
+	assert.EqualValues(t, "message: This is the test message - status_code: 404 - error: not_found - causes: []", err.Error())
 }
 
 func TestNewBadRequestRESTError(t *testing.T) {
 	err := NewBadRequestRESTError("This is the test message")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusBadRequest, err.StatusCode)
-	assert.EqualValues(t, "This is the test message", err.Message)
-	assert.EqualValues(t, "BAD REQUEST", err.Error)
+	assert.EqualValues(t, http.StatusBadRequest, err.StatusCode())
+	assert.EqualValues(t, "This is the test message", err.Message())
+	assert.EqualValues(t, "message: This is the test message - status_code: 400 - error: bad_request - causes: []", err.Error())
+}
+
+func TestNewUnauthorizedRESTError(t *testing.T) {
+	err := NewUnauthorizedRESTError("This is the test message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusUnauthorized, err.StatusCode())
+	assert.EqualValues(t, "This is the test message", err.Message())
+	assert.EqualValues(t, "message: This is the test message - status_code: 401 - error: unauthorized - causes: []", err.Error())
 }
