d163d172bbd776d293b56a40b6af3407a92bea90
Adam Vu <gvu0110@gmail.com>
2021-05-27 23:59:48 -0400

    Updated exported fields

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -13,70 +13,70 @@ type RESTError interface {
 
 // restError struct is an custom REST error is used for entire microservice
 type restError struct {
-	statusCode int           `json:"status_code"`
-	message    string        `json:"message"`
-	errorCode  string        `json:"error"`
-	causes     []interface{} `json:"causes"`
+	ErrorStatusCode int           `json:"status_code"`
+	ErrorMessage    string        `json:"message"`
+	ErrorCode       string        `json:"error"`
+	ErrorCauses     []interface{} `json:"causes"`
 }
 
 // Implement built-in Error go function
 func (e restError) Error() string {
-	return e.errorCode
+	return e.ErrorCode
 }
 
 func (e restError) StatusCode() int {
-	return e.statusCode
+	return e.ErrorStatusCode
 }
 
 func (e restError) Message() string {
-	return e.message
+	return e.ErrorMessage
 }
 
 func (e restError) Causes() []interface{} {
-	return e.causes
+	return e.ErrorCauses
 }
 
-func NewRESTError(message string, statusCode int, errorCode string, causes []interface{}) RESTError {
+func NewRESTError(message string, statusCode int, errCode string, causes []interface{}) RESTError {
 	return restError{
-		statusCode: statusCode,
-		message:    message,
-		errorCode:  errorCode,
-		causes:     causes,
+		ErrorStatusCode: statusCode,
+		ErrorMessage:    message,
+		ErrorCode:       errCode,
+		ErrorCauses:     causes,
 	}
 }
 
 // NewBadRequestRESTError creates a new bad request REST error
 func NewBadRequestRESTError(message string) RESTError {
 	return restError{
-		statusCode: http.StatusBadRequest,
-		message:    message,
-		errorCode:  "bad_request",
+		ErrorStatusCode: http.StatusBadRequest,
+		ErrorMessage:    message,
+		ErrorCode:       "bad_request",
 	}
 }
 
 // NewNotFoundRESTError creates a new not found REST error
 func NewNotFoundRESTError(message string) RESTError {
 	return restError{
-		statusCode: http.StatusNotFound,
-		message:    message,
-		errorCode:  "not_found",
+		ErrorStatusCode: http.StatusNotFound,
+		ErrorMessage:    message,
+		ErrorCode:       "not_found",
 	}
 }
 
 func NewUnauthorizedRESTError(message string) RESTError {
 	return restError{
-		statusCode: http.StatusUnauthorized,
-		message:    message,
-		errorCode:  "unauthorized",
+		ErrorStatusCode: http.StatusUnauthorized,
+		ErrorMessage:    message,
+		ErrorCode:       "unauthorized",
 	}
 }
 
 // NewInternalServerError creates a new internal server REST error
 func NewInternalServerRESTError(message string, err error) RESTError {
-	return restError{
-		statusCode: http.StatusInternalServerError,
-		message:    message,
-		errorCode:  "internal_server_error",
-		causes:     []interface{}{err.Error()},
+	return &restError{
+		ErrorStatusCode: http.StatusInternalServerError,
+		ErrorMessage:    message,
+		ErrorCode:       "internal_server_error",
+		ErrorCauses:     []interface{}{err.Error()},
 	}
 }
